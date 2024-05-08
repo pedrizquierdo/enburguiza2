93bcab0b114cc7b1b00feb2251b3c371abe40a89
pedrizquierdo <pedrizquierdo@localhost>
2024-05-08 11:46:40 -0700

    Versión Inicial

---
+++ src/main/java/mx/itson/enburguiza/entities/Client.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.enburguiza.entities;
+
+/**
+ *
+ * @author pedrizquierdo
+ */
+public class Client {
+    
+    private String address;
+    
+    
+    
+}
+

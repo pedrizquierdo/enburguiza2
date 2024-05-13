dd3a5fec6bcf881a02c32e7034f45537b7a4d91e
pedrizquierdo <pedrizquierdo@localhost>
2024-05-12 19:05:12 -0700

    Agregué las variables de instancia de la clase Client y las encapsulé

--- src/main/java/mx/itson/enburguiza/entities/Client.java
+++ src/main/java/mx/itson/enburguiza/entities/Client.java
@@ -9,11 +9,83 @@ package mx.itson.enburguiza.entities;
  * @author pedrizquierdo
  */
 public class Client {
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return the coordinates
+     */
+    public String getCoordinates() {
+        return coordinates;
+    }
+
+    /**
+     * @param coordinates the coordinates to set
+     */
+    public void setCoordinates(String coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
     
     private String address;
-    private String pedro;
+    private String name;
+    private String phoneNumber;
+    private String coordinates;
+    private String email;
     
-    //Hola bebé
-    //hola mi amor bello preciosos guapo mi vida bello 
+     
 }
 

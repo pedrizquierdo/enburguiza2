bad687238c25e9221367f1ec59d5dacfc0d2a4e5
pedrizquierdo <pedrizquierdo@localhost>
2024-05-12 18:52:54 -0700

    Agregué las variables de instancia y el método para deserializar

--- src/main/java/mx/itson/enburguiza/entities/App.java
+++ src/main/java/mx/itson/enburguiza/entities/App.java
@@ -4,10 +4,69 @@
  */
 package mx.itson.enburguiza.entities;
 
+import com.google.gson.Gson;
+
 /**
  *
  * @author pedrizquierdo
  */
 public class App {
+
+    /**
+     * @return the client
+     */
+    public Client getClient() {
+        return client;
+    }
+
+    /**
+     * @param client the client to set
+     */
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    /**
+     * @return the order
+     */
+    public Order getOrder() {
+        return order;
+    }
+
+    /**
+     * @param order the order to set
+     */
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    /**
+     * @return the item
+     */
+    public Item getItem() {
+        return item;
+    }
+
+    /**
+     * @param item the item to set
+     */
+    public void setItem(Item item) {
+        this.item = item;
+    }
     
+    private Client client;
+    private Order order;
+    private Item item;
+    
+    
+    public App deserializar (String json) {
+        App app = new App ();
+        try {
+            app= new Gson().fromJson(json, App.class);
+            
+    } catch(Exception ex) {
+            System.err.println("Ocurrio un error al deserializar" + ex.getMessage()); 
+    }
+     return app;
+    }
 }

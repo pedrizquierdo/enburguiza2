9430bcfebae058730613883c54793a9baf83eacc
luismorellb <luismorellbrbn0805@gmail.com>
2024-05-13 15:31:54 -0700

    Se documentó la clase Operation.

--- src/main/java/mx/itson/enburguiza/business/Operation.java
+++ src/main/java/mx/itson/enburguiza/business/Operation.java
@@ -10,9 +10,22 @@ package mx.itson.enburguiza.business;
  */
 public class Operation {
     
+    /**
+     * Calculates the sum of two numbers.
+     * @param num1 The first number to plus.
+     * @param num2 The second number to plus.
+     * @return The sum of the two numbers.
+     */
+    
     public static double add(double num1, double num2){
         return num1 + num2;
     }
+    
+    /**
+     * Calculates taxes on a given price.
+     * @param price The price to which taxes will be applied.
+     * @return The total price after taxes (original price plus taxes).
+     */
     
     public double taxes(double price){
         double taxes = (price * 0.16) + price;

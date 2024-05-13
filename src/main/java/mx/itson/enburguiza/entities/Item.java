28179002e4ca59270dd989318c1700a3bf21bcbd
UlisesPai <ulibpa2305@gmail.com>
2024-05-12 17:56:45 -0800

    encapsule los campos de las variables de instancia que se van a deserealizar de la clase "Item"

--- src/main/java/mx/itson/enburguiza/entities/Item.java
+++ src/main/java/mx/itson/enburguiza/entities/Item.java
@@ -9,6 +9,48 @@ package mx.itson.enburguiza.entities;
  * @author gals0
  */
 public class Item {
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * @return the price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
     
     private String name;
     private int quantity;

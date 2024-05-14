6819b55a1b5915df1bfb36ab90eacaaeff76d3a1
luismorellb <luismorellbrbn0805@gmail.com>
2024-05-13 19:47:34 -0700

    Se modificó la interfaz del Frame.

--- src/main/java/mx/itson/enburguiza/ui/EnburguizaFrame.java
+++ src/main/java/mx/itson/enburguiza/ui/EnburguizaFrame.java
@@ -106,9 +106,10 @@ public class EnburguizaFrame extends javax.swing.JFrame {
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addComponent(labelAddress, javax.swing.GroupLayout.PREFERRED_SIZE, 848, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(labelAddress, javax.swing.GroupLayout.PREFERRED_SIZE, 836, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -126,7 +127,7 @@ public class EnburguizaFrame extends javax.swing.JFrame {
                 buttonJsonActionPerformed(evt);
             }
         });
-        jPanel1.add(buttonJson, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 700, 80, 30));
+        jPanel1.add(buttonJson, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 610, 80, 30));
 
         labelRestaurant.setFont(new java.awt.Font("Arial", 1, 48)); // NOI18N
         labelRestaurant.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -149,7 +150,7 @@ public class EnburguizaFrame extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(tableItem);
 
-        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-40, 310, 770, 220));
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 310, 770, 220));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
